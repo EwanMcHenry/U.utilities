401ed0e34b7b66a2c5ea60b1f2cd0c392739143e
EwanMcHenry <ewan.mchenry@outlook.com>
2024-08-19 12:18:20 +0100

    commit 1

---
+++ R/getting package built and repro made etc.R
@@ -0,0 +1,22 @@
+
+library(devtools)
+setwd("")
+
+test("D:\\Users\\Ewan McHenry\\OneDrive - the Woodland Trust\\Analysis\\U.utilities")
+document("D:\\Users\\Ewan McHenry\\OneDrive - the Woodland Trust\\Analysis\\U.utilities")
+build("D:\\Users\\Ewan McHenry\\OneDrive - the Woodland Trust\\Analysis\\U.utilities")
+install("D:\\Users\\Ewan McHenry\\OneDrive - the Woodland Trust\\Analysis\\U.utilities")
+check("D:\\Users\\Ewan McHenry\\OneDrive - the Woodland Trust\\Analysis\\U.utilities")
+
+library(U.utilities)
+pad.lim(1:5)
+
+
+
+
+# bash
+git remote add origin https://github.com/EwanMcHenry/U.utilities.git
+
+
+# R
+gitcreds::gitcreds_set()
