598a46b68d3d50489ad08d7a150071df4e0ce0b5
EwanMcHenry <ewan.mchenry@outlook.com>
2024-08-20 14:45:26 +0100

    initial commit

---
+++ R/data_documentation.R
@@ -0,0 +1,134 @@
+#' GIS Working Directory
+#'
+#' This is the path to the GIS working directory used across projects.
+#'
+#' @format A character vector representing the file path.
+#' @examples
+#' gis.wd
+"gis.wd"
+
+#' Main Working Directory
+#'
+#' This is the path to the main working directory used across projects.
+#'
+#' @format A character vector representing the file path.
+#' @examples
+#' main.wd
+"main.wd"
+
+#' CARs Factfile Directory
+#'
+#' This is the path to the CARs factfile directory.
+#'
+#' @format A character vector representing the file path.
+#' @examples
+#' cars.factfile.direct
+"cars.factfile.direct"
+
+#' Treescape Names
+#'
+#' A vector containing the names of treescapes.
+#'
+#' @format A character vector with the names of treescapes.
+#' @examples
+#' ts.names
+"ts.names"
+
+#' Treescape Abbreviations
+#'
+#' A vector containing the abbreviations of treescapes.
+#'
+#' @format A character vector with the abbreviations of treescapes.
+#' @examples
+#' ts.abbrv
+"ts.abbrv"
+
+#' LCM Names for Treescapes
+#'
+#' A vector containing the LCM names for treescapes.
+#'
+#' @format A character vector with the LCM names.
+#' @examples
+#' ts.lcm.names
+"ts.lcm.names"
+
+#' Nice Names for Treescapes
+#'
+#' A vector containing the nice names of treescapes.
+#'
+#' @format A character vector with the nice names.
+#' @examples
+#' ts.nice.names
+"ts.nice.names"
+
+#' Treescape Names with All Option
+#'
+#' A vector containing the LCM names for treescapes including an 'All Treescapes' option.
+#'
+#' @format A character vector with the names.
+#' @examples
+#' this.andAll.tss
+"this.andAll.tss"
+
+#' Nice Names for Treescapes with All Option
+#'
+#' A vector containing the nice names of treescapes including an 'All Treescapes' option.
+#'
+#' @format A character vector with the nice names.
+#' @examples
+#' ts.andAll.nice.names
+"ts.andAll.nice.names"
+
+#' LCM Names for Treescapes with All Option
+#'
+#' A vector containing the LCM names for treescapes including an 'All Treescapes' option.
+#'
+#' @format A character vector with the LCM names.
+#' @examples
+#' ts.andAll.lcm.names
+"ts.andAll.lcm.names"
+
+#' England CARs Names
+#'
+#' A vector containing the names of England CARs regions.
+#'
+#' @format A character vector with the names of England CARs regions.
+#' @examples
+#' england.car.names
+"england.car.names"
+
+#' Country Names
+#'
+#' A vector containing the names of countries (England, Northern Ireland, Scotland, Wales).
+#'
+#' @format A character vector with the names of countries.
+#' @examples
+#' countries.names
+"countries.names"
+
+#' CEH Color Palette
+#'
+#' A color palette used for CEH land cover types.
+#'
+#' @format A character vector of hex color codes.
+#' @examples
+#' ceh.col.pallette
+"ceh.col.pallette"
+
+#' Full Habitat Types from CEH
+#'
+#' A vector containing full habitat types from the CEH dataset.
+#'
+#' @format A factor with levels corresponding to habitat types.
+#' @examples
+#' ceh.full.habtype
+"ceh.full.habtype"
+
+#' Concatenated Habitat Types
+#'
+#' A vector containing concatenated habitat types from the CEH dataset.
+#'
+#' @format A character vector of habitat types.
+#' @examples
+#' ceh.concatted1.habtype
+"ceh.concatted1.habtype"
