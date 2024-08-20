5f6457cc03d21960d9e822107b57ef023d80f5f9
EwanMcHenry <ewan.mchenry@outlook.com>
2024-08-20 14:45:47 +0100

    portable name

---
+++ R/getting-package-built-and-repro-made-etc.R
@@ -0,0 +1,28 @@
+# running the code needed to build a package and push it to github
+# # thiscauses an infinite loop becasue thiscoderuns itself! lol, so im gonna comment it all out, jsut run each bit seperate
+
+
+
+# devtools::document()
+# devtools::check()
+# devtools::build()
+# devtools::install()
+
+
+# build("D:\\Users\\Ewan McHenry\\OneDrive - the Woodland Trust\\Analysis\\U.utilities")
+# install("D:\\Users\\Ewan McHenry\\OneDrive - the Woodland Trust\\Analysis\\U.utilities")
+# check("D:\\Users\\Ewan McHenry\\OneDrive - the Woodland Trust\\Analysis\\U.utilities")
+#
+# library(U.utilities)
+# pad.lim(1:5)
+#
+# # bash
+# #git remote add origin https://github.com/EwanMcHenry/U.utilities.git
+#
+#
+# # R
+# gitcreds::gitcreds_set()
+#
+#
+# devtools::install_github("EwanMcHenry/U.utilities")
+
